412779b662a2577ab70e078814cb4c2340f21511
余仁哲 <117014879+Yrzhe@users.noreply.github.com>
2025-08-12 14:27:00 +0800

    Initial Commit

---
+++ safeaway/safeawayApp.swift
@@ -0,0 +1,17 @@
+//
+//  safeawayApp.swift
+//  safeaway
+//
+//  Created by 仁哲 Yu on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct safeawayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
